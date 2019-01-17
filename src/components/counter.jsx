08a7e0652fd5445c1d0b8ce4f30d926fa38cec53
ahmadthoyib <thoyibghofur16@gmail.com>
2019-01-17 11:48:19 +0700

    rendering classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,14 +4,20 @@ class Counter extends Component {
     state = {
         Count: 0  
     };
-    render() { 
+    render() {
         return (
         <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className={this.getbadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-danger btn-sm">increment</button>
         </div>
         );
     }
+    getbadgeClasses() {
+        let classes = "badge m-2 badge";
+        classes += this.state.Count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
     formatCount(){
         const {Count} = this.state;
         return Count === 0 ? "zero" : Count;
