0cab02d4841daef1d80e29116363b753413ffddb
ahmadthoyib <thoyibghofur16@gmail.com>
2019-01-17 10:37:21 +0700

    first component

---
+++ src/components/counter.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    render() { 
+        return (
+        <React.Fragment>
+        <h1>hello world</h1>
+        <button>increment</button>
+        </React.Fragment>
+        );
+    }
+}
+ 
+export default Counter;
